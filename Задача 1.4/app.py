cd0a9febc54ede9607ce26cc3f988dfbf52a893b
seregakriwoy <sergey.ol.shev@gmail.com>
2026-03-14 15:21:31 +0300

    updated test

--- Задача 1.4/app.py
+++ Задача 1.4/app.py
@@ -4,7 +4,12 @@ from models import User
 app = FastAPI()
 first_user = User(name="Шевалдышев Сергей", id=1)
 
+users_db = []
+
+@app.post("/users")
+async def post_users(user: User):
+    users_db.append(user)
 
 @app.get("/user")
 async def get_user():
-    return first_user
+    return users_db
